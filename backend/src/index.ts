b748a0af7fbeb05fe472f9c4387b31a31affb65b
jylux <gkalejaiye2@gmail.com>
2025-03-17 07:50:49 +0100

    modified backend code to use port 10000

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,11 +11,13 @@ dotenv.config();
 
 // Create Express application
 const app = express();
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 // Configure CORS
 app.use(
   cors({
+    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
     origin: process.env.FRONTEND_URL || "https://nipex-prequal-sup-creation-v7.onrender.com",
     credentials: true, // Critical for cookies to work with cross-origin requests
     methods: ["GET", "POST", "PUT", "DELETE"],
