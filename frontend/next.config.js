a7658bf031dbdaa675a013429cb330e29f98d2b9
jylux <gkalejaiye2@gmail.com>
2025-03-18 00:43:13 +0100

    modified next.config.js and package.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,12 @@
-module.exports = { 
-  reactStrictMode: true 
-}; 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // This helps with routing in static deployments
+  trailingSlash: true
+};
+
+module.exports = nextConfig;
